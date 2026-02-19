14ac8d00a68075182eef94b1fec82eb7bc40c1d6
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2026-02-19 13:50:06 -0300

    condicionantes

---
+++ week1/condicionantes.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (void)
+{
+
+/*if (x < y)
+{
+    printf("x é menor que y\n");
+}
+else
+{
+    printf("x não é menor que y\n");
+}
+*/
+
+if (x < y)
+{
+    printf("x é menor que y\n");
+}
+else if (x > y)
+{
+    printf("x é maior que y\n");
+}
+else //if (x == y) não precisa, economiza tempo e energia
+{
+    printf("x é igual a y\n");
+}
+
+}
+
